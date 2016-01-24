bb2c10250544c002f6d61c3b3ac5f177c464e9a9
imparikh <parikh.i.m@gmail.com>
2016-01-23 20:00:49 -0500

    cleaning up everything but stacked bar

--- donutChart.js
+++ donutChart.js
@@ -1,23 +1,40 @@
-var data = [ {name: "loaded", value: 64},
-			{name: "unloaded", value:  36}];
+//we just have to feed in a data variable that looks like this:
+var data = [{
+        name: "loaded",
+        value: 64
+    },
+    {
+        name: "unloaded",
+        value: 36
+    }];
 
-var margin = {top: 20, right: 20, bottom: 20, left: 20};
-	width = 400 - margin.left - margin.right;
-	height = width - margin.top - margin.bottom;
+//margins sort of weird with these svg's...
+
+var margin = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20
+};
+
+//adjust 400 as desired. Difficult to make it based of % bc this is js
+width = 400 - margin.left - margin.right;
+height = width - margin.top - margin.bottom;
 
 var chart = d3.select("body")
-				.append('svg')
-			    .attr("width", width + margin.left + margin.right)
-			    .attr("height", height + margin.top + margin.bottom)
-			   .append("g")
-    			.attr("transform", "translate(" + ((width/2)+margin.left) + "," + ((height/2)+margin.top) + ")");
+    .append('svg')
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + ((width / 2) + margin.left) + "," + ((height / 2) + margin.top) + ")");
 
 var status = data[0].value;
 
 var radius = Math.min(width, height) / 2;
 
+//we can make it not blue if you want to, just edit these.
 var color = d3.scale.ordinal()
-	.range(["#3399FF", "#5DAEF8", "#86C3FA", "#ADD6FB", "#D6EBFD"]);
+    .range(["#3399FF", "#5DAEF8", "#86C3FA", "#ADD6FB", "#D6EBFD"]);
 
 var arc = d3.svg.arc()
     .outerRadius(radius)
@@ -25,24 +42,28 @@ var arc = d3.svg.arc()
 
 var pie = d3.layout.pie()
     .sort(null)
-    .startAngle(1.1*Math.PI)
-    .endAngle(3.1*Math.PI)
-    .value(function(d) { return d.value; });
+    .startAngle(1.1 * Math.PI)
+    .endAngle(3.1 * Math.PI)
+    .value(function (d) {
+        return d.value;
+    });
 
 var g = chart.selectAll(".arc")
-  .data(pie(data))
-.enter().append("g")
-  .attr("class", "arc");
+    .data(pie(data))
+    .enter().append("g")
+    .attr("class", "arc");
 
 g.append("path")
-  .style("fill", function(d) { return color(d.data.name); })
-  .attr("d", arc);
+    .style("fill", function (d) {
+        return color(d.data.name);
+    })
+    .attr("d", arc);
 
 chart.append("text")
-   .attr("text-anchor", "middle")
-   .text(status);
+    .attr("text-anchor", "middle")
+    .text(status);
 
-//necessary css
+//necessary css - divides the two colors. border.
 //path {
 //    stroke: #ffffff;
 //    stroke-width: 3px;
--- metrics-graphics-2.8.0/test.js
+++ metrics-graphics-2.8.0/test.js
@@ -1,12 +1,25 @@
+//JSON in following format:
+//[{
+//    "time": "10",
+//    "sightings": 6
+//  },
+//  {
+//    "time": "20",
+//    "sightings": 8
+//  }
+//]
+
 d3.json('data/testData.json', function (data) {
     MG.data_graphic({
         title: "Response Time",
         description: "View response times of your endpoint",
         data: JSON.parse($('.data textarea').val()),
+        //markers can be removed if so desired
         markers: [{
             'time': 30,
             'label': 'benchmark'
         }],
+        //width and height can be changed
         width: 400,
         height: 250,
         target: ".result",
